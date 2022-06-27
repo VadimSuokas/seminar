471ae33811994602279e3aaf84321a5963cd8bbe
Vadim <suokasvrn@gmail.com>
2022-06-27 20:33:33 +0300

    Прописали метод

---
+++ seminar2/Program.cs
@@ -0,0 +1,11 @@
+﻿int CutNumber()
+{
+    int num = new Random().Next(100,1000);
+    Console.Write("Current random number is " + num);
+
+    int sotni = num / 100;
+    int ed = num % 10;
+
+    int result = sotni * 10 + ed;
+    return result;
+}
