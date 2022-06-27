f31a04f6176e14db2854d133c97ed5d88818c40d
Vadim <suokasvrn@gmail.com>
2022-06-27 21:35:47 +0300

    Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,14 +1,25 @@
 ﻿int CutNumber()
 {
-    int num = new Random().Next(100,1000);
+    int num = new Random().Next(10,100);
     Console.Write("Current random number is " + num);
 
-    int sotni = num / 100;
+    int des = num / 10;
     int ed = num % 10;
 
     int result = sotni * 10 + ed;
     return result;
 }
 
-int number = CutNumber();
-Console.Write(" Result number is " + number); 
+void IsQuad(int num1, int num2);
+{
+    int quad1 = num1 * num1;
+    int quad2 = num2 * num2;
+    if(num1 == quad2)
+        Console.WriteLine("Yes");
+    else
+        if(num2 == quad1)
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("NO");
+}
+
