b26ff4a137523fe15728009b7178dac4a3e2fdd3
Vadim <suokasvrn@gmail.com>
2022-07-04 20:14:05 +0300

    FindSum

---
+++ seminar4/Program.cs
@@ -0,0 +1,13 @@
+﻿int FindSum(int a)
+{
+    int sum = 0;
+
+    for(int current = 1; current <= a; current++) 
+        sum += current; // sum = sum + current;
+    return sum;
+}
+
+Console.WriteLine("Input positive intrger number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of numbers from 1 to {num} is {FindSum(num)}");
