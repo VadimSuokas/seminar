f15e18488b8b872bedffb062f7d939d5d41eb290
Vadim <suokasvrn@gmail.com>
2022-07-07 20:21:46 +0300

    CreateArray

--- seminar5/Program.cs
+++ seminar5/Program.cs
@@ -1,10 +1,24 @@
 ﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму положительных элементов массива.
 
+/*
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] newArray = new int[size]; // Инициализирует массив newArray
 
     for(int i = 0; i < size; i++)
         newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+*/
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size]; 
+    Console.Write("Creating array here: ");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
     return newArray;
 }
