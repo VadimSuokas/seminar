ea3e6c468a75e511daaeeae7832446f229d55af0
Vadim <suokasvrn@gmail.com>
2022-07-11 21:36:17 +0300

    Две задачи с семинара

--- seminar6/Program.cs
+++ seminar6/Program.cs
@@ -40,7 +40,7 @@ for(int i = 0; i < myArray.Length; i++)
 // 9 / 2 -> 6 1
 
 
-
+/*
 string ChangeDigitSystem (int num)
 {
     string resultNumber = string.Empty;
@@ -50,4 +50,41 @@ string ChangeDigitSystem (int num)
         resultNumber = num % 2 + resultNumber;
         num /= 2; // num = num / 2;
     }
+
+    return resultNumber;
 }
+
+Console.WriteLine(ChangeDigitSystem(18));
+*/
+
+// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+void Treugolnik(int a,int b, int c)
+{
+    if(a < b + c && b < c + a && c < a + b) 
+        Console.Writeline("Треугольник существует");
+    else
+        Console.WriteLine("Такого треугольника не может существовать");
+
+}
+int a = Convert.ToInt32(Console.Readline());
+int b = Convert.ToInt32(Console.Readline());
+int c = Convert.ToInt32(Console.Readline());
+Treugolnik(a,b,c);
+
+
+// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+int[] Fibonacci(int N)
+{
+    int[] newArray = new int[N];
+    Console.WriteLine("Creating array here: ");
+    newArray[0] = 0;
+    newArray[1] = 1;
+    for(int i = 2; i < N; i++)
+        newArray[i] = newArray[i - 1] + newArray[i - 2];
+    return newArray;
+}
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.Readline());
+
+Fibonacci(num);
