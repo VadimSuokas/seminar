056f8120f483c54069ecba5637f3073f7613a013
Vadim <suokasvrn@gmail.com>
2022-07-14 20:29:30 +0300

    двумерный массив

--- seminar7/Program.cs
+++ seminar7/Program.cs
@@ -18,18 +18,22 @@ void Show2dArray(int[,] array)
         for(int j= 0; j < array.GetLength(1); j++)
             Console.Write(array[i,j] + " ");
         
-        Console.Writeline();
+        Console.WriteLine();
     }
 }
 
 Console.Write("Input number of rows ");
-int m = Convert.ToInt32(Console.Readline());
+int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of columns ");
-int n = Convert.ToInt32(Console.Readline());
+int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of value ");
-int min = Convert.ToInt32(Console.Readline());
+int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of value ");
-int max = Convert.ToInt32(Console.Readline());
+int max = Convert.ToInt32(Console.ReadLine());
 
 int[,] myArray = CreateRandom2dArray(m,n,min,max);
 Show2dArray(myArray);
+
+// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экран.
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
